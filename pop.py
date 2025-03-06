13ccfd6cc34fd2702b15424a200f5eb181599d8f
Mola-klp <arrafimatin5566@gmail.com>
2025-03-06 08:36:44 +0700

    Add files via upload

---
+++ pop.py
@@ -0,0 +1,8 @@
+# Meminta pengguna memasukkan suhu dalam Celsius
+celsius = float(input("Masukkan suhu dalam derajat Celsius: "))
+
+# Mengonversi ke Fahrenheit
+fahrenheit = (celsius * 9/5) + 32
+
+# Menampilkan hasil
+print(celsius, "derajat Celsius =", fahrenheit, "derajat Fahrenheit")
